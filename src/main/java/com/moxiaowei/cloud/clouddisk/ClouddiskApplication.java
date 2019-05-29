4d51da5b9732f5e9ff09f87970e88d8d85c0883c
CvCn <876247638@qq.com>
2019-05-29 15:05:40 +0800

    first commit

---
+++ src/main/java/com/moxiaowei/cloud/clouddisk/ClouddiskApplication.java
@@ -0,0 +1,13 @@
+package com.moxiaowei.cloud.clouddisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouddiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClouddiskApplication.class, args);
+    }
+
+}
